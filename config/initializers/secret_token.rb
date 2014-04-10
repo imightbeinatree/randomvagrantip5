bf083f0df685791f0c579b54be7f4768ea89e815
Michael Orr <michael@cloudspace.com>
2014-04-10 02:35:38 +0000

    initial project generation using the Cloudspace Rails 4 Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Randomvagrantip5::Application.config.secret_key_base = '36f700f01b15105fa1cbe41416f457f8f5f6c7717b5bac06e4e9f1094db31092658ab4346c1d5370e4aee7ca43d14e869664acbda6ab771a0b4bc15bd4bc3056'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Randomvagrantip5::Application.config.session_store :cookie_store, key: '_randomvagrantip5_session'
